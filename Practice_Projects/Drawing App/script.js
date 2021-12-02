0728529ac5a6f023881a14507c8d67267c966d07
Keely Bailey <keelybailey3@gmail.com>
2021-12-01 20:23:55 -0600

    Toolbox controls

--- Practice_Projects/Drawing App/script.js
+++ Practice_Projects/Drawing App/script.js
@@ -1,7 +1,13 @@
 const canvas = document.getElementById("canvas");
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+const sizeEl = document.getElementById("size");
+const colorEl = document.getElementById("color");
+const clearEl = document.getElementById("clear");
+
 const ctx = canvas.getContext('2d');
 
-let size = 20;
+let size = 10;
 let isPressed = false
 let color = 'black';
 let x
@@ -47,7 +53,32 @@ function drawLine(x1, y1, x2, y2) {
     ctx.strokeStyle = color
     ctx.lineWidth = size * 2
     ctx.stroke()
-}
+};
 
-// drawCircle(100, 200);
-// drawLine(300, 300, 300, 500);
+function updateSizeOnScreen() {
+    sizeEl.innerText = size;
+};
+
+increaseBtn.addEventListener('click', () => {
+    size += 5
+
+    if(size > 50) {
+        size = 50;
+    }
+
+updateSizeOnScreen()
+});
+
+decreaseBtn.addEventListener('click', () => {
+    size -= 5
+
+    if(size < 5) {
+        size = 5;
+    }
+
+updateSizeOnScreen()
+});
+
+colorEl.addEventListener('change', (e) => color = e.target.value);
+
+clearEl.addEventListener('click', () => ctx.clearRect(0, 0, canvas.width, canvas.height))
